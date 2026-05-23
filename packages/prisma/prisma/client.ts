a45b1862adc9b990e7cf6b8b12aabfa906561474
freddster14 <freddycarrillo951@gmail.com>
2026-05-23 14:42:39 -0500

    Remove console.log statement from Prisma client

--- packages/prisma/prisma/client.ts
+++ packages/prisma/prisma/client.ts
@@ -5,7 +5,6 @@ import { PrismaClient } from "../generated/prisma/client.js";
 const connectionString = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_DATABASE_URL 
   : process.env.DATABASE_URL;
-  console.log(connectionString)
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
