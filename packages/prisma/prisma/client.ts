740d0ac1d618077e540747914f704ac652a98bfc
freddster14 <freddycarrillo951@gmail.com>
2026-05-23 14:41:19 -0500

    Remove commented-out console.log statement from Prisma client for cleaner code.

--- packages/prisma/prisma/client.ts
+++ packages/prisma/prisma/client.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
-console.log(process.env.NODE_ENV, process.env.TEST_DATABASE_URL )
 const connectionString = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_DATABASE_URL 
   : process.env.DATABASE_URL;
